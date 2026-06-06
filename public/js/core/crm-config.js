fab0506f3d977a9b3619edf774c9e680cbdc3cfa
Samuel Vinsansi <samuel.santos1vg@gmail.com>
2026-06-06 08:12:55 -0300

    Delete public directory

--- public/js/core/crm-config.js
+++
@@ -1,7 +0,0 @@
-window.CRM_CONFIG = Object.freeze({
-  supabaseUrl: 'https://txyknazfufashgzlxkqh.supabase.co',
-  supabaseAnonKey: 'sb_publishable_ClGVAmaiS4tNWe8W_4EPew_aPvAzK0E',
-  evolutionUrl: 'https://evolution.samuelvinsansi.com.br',
-  loginRedirectPath: '/index.html',
-  appVersion: 'fase-2'
-});
