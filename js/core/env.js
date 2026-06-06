8c0f2d722ce829006cf75985a1df50d41be66459
Samuel Vinsansi <samuel.santos1vg@gmail.com>
2026-06-05 21:58:30 -0300

    Delete js directory

--- js/core/env.js
+++
@@ -1,6 +0,0 @@
-// Copie este arquivo para env.js e preencha com o projeto Supabase DEV.
-window.CRM_ENV = {
-  SUPABASE_URL: 'https://txyknazfufashgzlxkqh.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_ClGVAmaiS4tNWe8W_4EPew_aPvAzK0E',
-  APP_ENV: 'dev'
-};
