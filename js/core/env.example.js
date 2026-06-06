2f047a13437fd5264ba1f6f421a2746022007c3a
Samuel Vinsansi <samuel.santos1vg@gmail.com>
2026-06-05 21:34:42 -0300

    Update env.example.js

--- js/core/env.example.js
+++ js/core/env.example.js
@@ -1,6 +1,6 @@
 // Copie este arquivo para env.js e preencha com o projeto Supabase DEV.
 window.CRM_ENV = {
-  SUPABASE_URL: 'https://SEU-PROJETO.supabase.co',
-  SUPABASE_ANON_KEY: 'SUA_ANON_KEY',
+  SUPABASE_URL: 'https://txyknazfufashgzlxkqh.supabase.co/rest/v1/',
+  SUPABASE_ANON_KEY: 'sb_publishable_ClGVAmaiS4tNWe8W_4EPew_aPvAzK0E',
   APP_ENV: 'dev'
 };
