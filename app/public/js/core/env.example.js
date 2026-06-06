2b7fd6046e4bce8c0db0cd1efda467226ada92a1
Samuel Vinsansi <samuel.santos1vg@gmail.com>
2026-06-05 21:22:52 -0300

    Delete app/public directory

--- app/public/js/core/env.example.js
+++
@@ -1,6 +0,0 @@
-// Copie este arquivo para env.js e preencha com o projeto Supabase DEV.
-window.CRM_ENV = {
-  SUPABASE_URL: 'https://SEU-PROJETO.supabase.co',
-  SUPABASE_ANON_KEY: 'SUA_ANON_KEY',
-  APP_ENV: 'dev'
-};
