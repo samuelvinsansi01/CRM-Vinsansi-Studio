e0d296ecf389fdbb1731b7ceec61b947e290d9c9
Samuel Vinsansi <samuel.santos1vg@gmail.com>
2026-06-21 14:07:54 -0300

    V131

--- public/js/modules/zz-v125-instagram-invalidar-no-redirect.js
+++
@@ -1,6 +0,0 @@
-/* V125 desativado pela V126.
-   A regra final agora está em zz-v126-atribuicao-invalidar-aprovar-base.js:
-   - invalidar em todas as abas da Atribuição = 6/Outro automático;
-   - Instagram aprova para Backlog, sem redirect.
-*/
-window.__V125_INSTAGRAM_INVALIDAR_NO_REDIRECT__='disabled-by-v126';
